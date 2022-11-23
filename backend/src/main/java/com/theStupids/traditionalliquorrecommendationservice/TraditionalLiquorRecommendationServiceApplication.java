d42d70650800deaa51541c41e36a23541cf414b2
byh9811 <byh9811@naver.com>
2022-11-24 03:35:32 +0900

    INITIATE: Initiate Spring
    1. 프로젝트 전체 구조 및 스프링 개발 환경을 세팅함.

---
+++ backend/src/main/java/com/theStupids/traditionalliquorrecommendationservice/TraditionalLiquorRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.theStupids.traditionalliquorrecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalLiquorRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraditionalLiquorRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/theStupids/traditionalliquorrecommendationservice/TraditionalLiquorRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theStupids.traditionalliquorrecommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraditionalLiquorRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
