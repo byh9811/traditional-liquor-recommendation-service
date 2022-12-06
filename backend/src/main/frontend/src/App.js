ad5517bdc38f345f57fdca40b5a69a8e6290bdf1
byh9811 <byh9811@naver.com>
2022-12-06 16:33:51 +0900

    FEAT: Create basic test code
    1. 스프링-리액트 연동이후 배포하여 확인해보기 위해 간단한 테스트 코드 작성.

--- backend/src/main/frontend/src/App.js
+++ backend/src/main/frontend/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div>
-      hello react app!
-    </div>
-  );
+    const [back, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/test')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            정상 작동중이니 나가주세요 : {back}
+        </div>
+    );
 }
 
 export default App;
