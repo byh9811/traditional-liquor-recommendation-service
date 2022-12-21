850e6e12ecd54d536616903a1736ea95867c3112
byh9811 <byh9811@naver.com>
2022-12-21 15:36:05 +0900

    FEAT: Create getting a liquor list feature
    1. 전통주 여러개를 검색하는 기능 개발.
    2. 관련 controller, service, repository와 DTO 수정 및 추가.
    3. Postman으로 테스트 수행 완료.

---
+++ backend/src/main/java/com/theStupids/traditionalliquorrecommendationservice/dto/controller/data/LiquorListDTO.java
@@ -0,0 +1,31 @@
+package com.theStupids.traditionalliquorrecommendationservice.dto.controller.data;
+
+import com.theStupids.traditionalliquorrecommendationservice.domain.LiquorList;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class LiquorListDTO {
+    private int id;
+    private String title;
+    private String img;
+    private int price;
+    private int volume;
+    private String type;
+    private double alcohol;
+    private List<String> food;
+
+    public LiquorListDTO(LiquorList liquorList, List<String> food) {
+        this.id = liquorList.getId();
+        this.title = liquorList.getTitle();
+        this.img = liquorList.getImg();
+        this.price = liquorList.getPrice();
+        this.volume = liquorList.getVolume();
+        this.type = liquorList.getType();
+        this.alcohol = liquorList.getAlcohol();
+        this.food = food;
+    }
+}
