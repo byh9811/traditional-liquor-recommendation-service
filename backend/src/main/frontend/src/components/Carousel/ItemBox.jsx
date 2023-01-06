9ab6e8c8c8227b9279f4899e4ffee2c2e261ddf4
byh9811 <byh9811@naver.com>
2023-01-06 23:44:13 +0900

    FIX: Fix main carousel
    1. 메인 캐러셀의 각 데이터를 서버에서 받아오도록 변경함.
    2. 이미지 표시 로직의 변화가 필요함.

--- backend/src/main/frontend/src/components/Carousel/ItemBox.jsx
+++ backend/src/main/frontend/src/components/Carousel/ItemBox.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import carousel from './Carousel.module.css';
+import {useNavigate} from "react-router-dom";
 
-export default function ItemBox({ url }) {
+export default function ItemBox({ id, url }) {
+    const navigate = useNavigate();
+    function goDetail(id) {
+        navigate(`/drinks/${id}`);
+    }
   return (
     <div className={carousel.item} >
-        <img src= { url } alt="" />
+        <img src= { url } alt=""
+             onClick={ () => {
+                 goDetail(id);
+             }}
+        />
     </div>
   )
 }
