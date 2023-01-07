bd19116a263997b5e0ec99217d367b62cade0289
byh9811 <byh9811@naver.com>
2023-01-07 15:31:01 +0900

    FIX: Fix img path value into yaml
    1. yml 파일의 경로를 PC에 따라 바꿀 수 있도록 @Value를 사용함.

--- backend/src/main/java/com/theStupids/traditionalliquorrecommendationservice/controller/ImageController.java
+++ backend/src/main/java/com/theStupids/traditionalliquorrecommendationservice/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.theStupids.traditionalliquorrecommendationservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -18,11 +19,14 @@ import java.io.IOException;
 
 @RestController
 public class ImageController {
+    @Value("${value.imgPath}")
+    private String imgPath;
+
     @GetMapping(value = "/display/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<byte[]> getImage(@PathVariable("id") String id, @RequestParam("w") int width, @RequestParam("h") int height) {
         byte[] imageByteArray = null;
         try {
-            String fileName = "C:\\img\\drinks\\" + id + ".BMP"; // 파일경로
+            String fileName = imgPath + id + ".BMP"; // 파일경로
             File file = new File(fileName);
             BufferedImage image = ImageIO.read(file);
             Image resizedImage = image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
