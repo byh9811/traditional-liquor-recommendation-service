5286acc3b7bbd81b939a8d9a36024bbf34ad82bf
byh9811 <byh9811@naver.com>
2023-01-07 16:17:46 +0900

    FIX: Fix some region name
    1. busan -> Busan
    2. Daegue -> Daegu

--- backend/src/main/frontend/src/pages/Shop.jsx
+++ backend/src/main/frontend/src/pages/Shop.jsx
@@ -4,7 +4,7 @@ import shop from './Shop.module.css';
 
 import Area from '../components/Area';
 export default function Shop() {
-  let area = ['Seoul','Gyeonggi','Incheon','Chungcheong','busan','Gyeongsang','Jeolla','Jeju','Gangwon', 'Daejeon', 'Gwangju', 'Daegue', 'Ulsan', 'Sejong'];
+  let area = ['Seoul','Gyeonggi','Incheon','Chungcheong','Busan','Gyeongsang','Jeolla','Jeju','Gangwon', 'Daejeon', 'Gwangju', 'Daegu', 'Ulsan', 'Sejong'];
   // let areaUrl = ['/stores?areaName="]; => 이거 안되니까 navigate로 가자...
   let key = [1,2,3,4,5,6,7,8];
 
