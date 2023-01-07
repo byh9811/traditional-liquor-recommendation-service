e0a0910ebe0c1ee5562957222c299cd99323ac3d
byh9811 <byh9811@naver.com>
2023-01-07 17:42:16 +0900

    FIX: Fix getting image api
    1. image api에 너비와 높이를 지정하는 기능을 삭제함.

--- backend/src/main/java/com/theStupids/traditionalliquorrecommendationservice/controller/ImageController.java
+++ backend/src/main/java/com/theStupids/traditionalliquorrecommendationservice/controller/ImageController.java
@@ -23,20 +23,16 @@ public class ImageController {
     private String imgPath;
 
     @GetMapping(value = "/display/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage(@PathVariable("id") String id, @RequestParam("w") int width, @RequestParam("h") int height) {
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") String id) {
         byte[] imageByteArray = null;
         try {
             String fileName = imgPath + id + ".BMP"; // 파일경로
             File file = new File(fileName);
             BufferedImage image = ImageIO.read(file);
-            Image resizedImage = image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
-            BufferedImage imgBuff = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            imgBuff.getGraphics().drawImage(resizedImage, 0, 0, new Color(0, 0, 0), null);
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(imgBuff, "jpg", baos);
+            ImageIO.write(image, "jpg", baos);
             imageByteArray = baos.toByteArray();
         } catch (FileNotFoundException e) {
-            System.out.println("no file");
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         } catch (IOException e) {
             return new ResponseEntity<>(null, HttpStatus.INSUFFICIENT_STORAGE);
