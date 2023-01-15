ffba95f2472891701b875d15b4ca1694cd48b42d
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-01-15 12:16:54 +0900

    pagination first and last button
    complete

--- backend/src/main/frontend/src/components/Pagination/bar.jsx
+++ backend/src/main/frontend/src/components/Pagination/bar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import bar from './Bar.module.css';
 import PageButton from './PageButton';
 export default function Bar() {
-  let pageNum = [1,2,3,4,5,6,7,8,9,10];
+  let pageNum = ['<<',1,2,3,4,5,6,7,8,9,10,">>"];
   return (
     <div className={ bar.bar__wrapper }>
       {
