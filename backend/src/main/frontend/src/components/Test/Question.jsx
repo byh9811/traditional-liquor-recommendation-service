23c9b8ace843535042029de5d9ea632079be3b52
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-01-26 00:22:53 +0900

    test error finish
    complete

--- backend/src/main/frontend/src/components/Test/Question.jsx
+++ backend/src/main/frontend/src/components/Test/Question.jsx
@@ -3,12 +3,14 @@ import test from '../../pages/Test.module.css';
 import Radio from './Radio';
 export default function Question({ question, answer, name, onChange, id }) {
     let questionList = [4, 3, 2, 1];
+    console.log(answer);
+    let answerList = ["사랑한다", "좋아한다", "그럭저럭", "싫어한다"];
   return (
     <div className={test.question}>
         <p>{ question }</p>
         
         <Radio 
-            answer = { answer }
+            answer = { answerList[0] }
             name = { name }
             onChange = { onChange }
             id = { id[0] }
@@ -17,21 +19,21 @@ export default function Question({ question, answer, name, onChange, id }) {
             value = { questionList[0] }
         />
         <Radio 
-            answer = { answer }
+            answer = { answerList[1] }
             name = { name }
             onChange = { onChange }
             id = { id[1] }
             value = { questionList[1] }
         />
         <Radio 
-            answer = { answer }
+            answer = { answerList[2] }
             name = { name }
             onChange = { onChange }
             id = { id[2] }
             value = { questionList[2] }
         />
         <Radio 
-            answer = { answer }
+            answer = { answerList[3] }
             name = { name }
             onChange = { onChange }
             id = { id[3] }
